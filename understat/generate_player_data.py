d86eb5d4c179d2e58c29a1f8875d9147f4aa3eac
geo9788 <george.metson@yahoo.co.uk>
2024-07-24 03:38:51 +0100

    Save football stuff

--- understat/generate_player_data.py
+++ understat/generate_player_data.py
@@ -1,1 +1,9 @@
 # Script to extract and save player data from understat
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from datetime import datetime
+import pandas as pd
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
